d94e9df36f9b6401158c568d745d97036d1eef2e
Solej77 <ing.josgalo@gmail.com>
2020-02-24 12:09:25 -0600

    Isolate the code from the database

--- components/user/network.js
+++ components/user/network.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const response = require('../../network/response');
+const controller = require('./controler');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  response.success(req, res, 'Todo OK', 201);
+  const lista = controller.list();
+  response.success(req, res, lista, 200);
 });
 
 module.exports = router;
